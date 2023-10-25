98317488a3426ba6af4eab2d43b8cd26cbbf9aeb
braveua <braveua@gmail.com>
2023-10-26 02:54:12 +0300

    add tags

--- notebook.py
+++ notebook.py
@@ -1,14 +1,27 @@
 import pymysql
 from settings import dbc
 
+tags = {"0": "Task",
+        "1": "Linux",
+        "2": "MySql",
+        "3": "Python"}
+
 
 class Notebook:
     def __init__(self):
+        dbc["cursorclass"] = pymysql.cursors.DictCursor
         try:
             self.db = pymysql.connect(**dbc)
         except pymysql.err.OperationalError as ex:
             print('Error:', ex)
             exit()
+
+    def get_tags(self, tag_):
+        res = []
+        for bit in range(0, 16):
+            if tag_ & (1 << bit):
+                res.append(tags[str(bit)])
+        return res
 
     def show_all(self):
         try:
@@ -17,15 +30,16 @@ class Notebook:
                 cursor.execute(qry)
                 resp = cursor.fetchall()
                 for rec in resp:
-                    print(rec)
+                    rec = dict(rec)
+                    print(rec, self.get_tags(rec["tag"]))
         except pymysql.err.ProgrammingError as ex:
             print('Error:', ex)
 
-    def add_note(self, subject, parentid=None, id_=None, content=None):
+    def add_note(self, subject, parentid=None, id_=None, content=None, tag=0):
 
         try:
             with self.db.cursor() as cursor:
-                cursor.callproc("addnote", (id_, parentid, subject, content))
+                cursor.callproc("addnote", (id_, parentid, subject, content, tag))
                 self.db.commit()
         except pymysql.err.ProgrammingError as ex:
             print('Insert Error:', ex)
@@ -33,7 +47,8 @@ class Notebook:
 
 if __name__ == "__main__":
     nb = Notebook()
-    nb.add_note("Записка #1")
-    nb.add_note("Записка #2")
-    nb.add_note(parentid=1, subject="пункт 1", content="описание")
+    # nb.add_note("Записка #1")
+    # nb.add_note("Записка #2")
+    # nb.add_note(parentid=1, subject="пункт 1", content="описание")
     nb.show_all()
+
